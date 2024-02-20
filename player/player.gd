f37b6582e6a64c136903a34aa105164ce4cdfc77
Eugen <72609351+eugencic@users.noreply.github.com>
2024-02-20 21:12:13 +0200

    add player movement and state

--- player/player.gd
+++ player/player.gd
@@ -6,35 +6,67 @@ class_name Player
 @onready var animation_player = $AnimationPlayer
 
 const GRAVITY: float = 500.0
+const RUN_SPEED: float = 120.0
+const MAX_FALL: float = 400.0
+const HURT_TIME: float = 0.3
+const JUMP_VELOCITY: float = -400.0
+
+enum PLAYER_STATE { IDLE, RUN, JUMP, FALL, HURT }
+
+var _state: PLAYER_STATE = PLAYER_STATE.IDLE
 
 func _physics_process(delta):
 	if is_on_floor() == false:
 		velocity.y += GRAVITY * delta
 		
+	get_input()
 	move_and_slide()
+	calculate_state()
 
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
-#
-## Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-#
-#
-#func _physics_process(delta):
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity.y += gravity * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-	#move_and_slide()
+func get_input() -> void:
+	
+	velocity.x = 0
+	
+	if Input.is_action_pressed("left") == true:
+		velocity.x = -RUN_SPEED
+	elif Input.is_action_pressed("right") == true:
+		velocity.x = RUN_SPEED
+		
+	if Input.is_action_just_pressed("jump") == true and is_on_floor() == true: 
+		velocity.y = JUMP_VELOCITY
+		
+	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL)
+	
+func calculate_state() -> void:
+	
+	if _state == PLAYER_STATE.HURT:
+		return
+
+	if is_on_floor() == true:
+		if velocity.x == 0:
+			set_state(PLAYER_STATE.IDLE)
+		else:
+			set_state(PLAYER_STATE.RUN)
+	else:
+		if velocity.y > 0:
+			set_state(PLAYER_STATE.FALL)
+		else: 
+			set_state(PLAYER_STATE.JUMP)
+			
+func set_state(new_state: PLAYER_STATE) -> void:
+	
+	if new_state == _state:
+		return
+		
+	_state = new_state
+	
+	match _state:
+		PLAYER_STATE.IDLE:
+			animation_player.play("idle")
+		PLAYER_STATE.RUN:
+			animation_player.play("run")
+		PLAYER_STATE.JUMP:
+			animation_player.play("jump")
+		PLAYER_STATE.FALL:
+			animation_player.play("fall")
+			

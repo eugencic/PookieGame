eaaefb6ed549a21ed1b38476aa2d32ecc2cb3644
dan-planable <danpogorevici@planable.io>
2024-02-27 23:14:41 +0200

    👌 Add animation for fruit pickup

--- fruit_pickup/fruit_pick_up.gd
+++ fruit_pickup/fruit_pick_up.gd
@@ -1,14 +1,32 @@
 extends Area2D
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
+const FRUITS: Array = ["melon", "kiwi", "banana", "cherry"]
+# increase for better animation
+const GRAVITY: float = 160.0
+const JUMP: float = -80.0
+const POINTS: int = 2
+
+var _start_y: float
+var _speed_y: float = JUMP
+var _stopped: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_start_y = global_position.y
+	animated_sprite_2d.play(FRUITS.pick_random())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if _stopped == true:
+		return
+		
+	position.y += _speed_y * delta
+	_speed_y += GRAVITY * delta
+	
+	if global_position.y > _start_y:
+		_stopped = true
 	
 	
 func kill_me() -> void:

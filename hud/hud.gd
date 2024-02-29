2c521d13299a925180d8baa2ad7f07131b24bb4d
dan-planable <danpogorevici@planable.io>
2024-02-29 21:12:12 +0200

    👌 Add hearts reduction

--- hud/hud.gd
+++ hud/hud.gd
@@ -3,11 +3,15 @@ extends Control
 @onready var color_rect = $ColorRect
 @onready var vb_level_complete = $ColorRect/LevelComplete
 @onready var vb_game_over = $ColorRect/GameOver
+@onready var hb_hearts = $MC/HB/HB_Hearts
 
+var _hearts: Array
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_hearts = hb_hearts.get_children()
 	SignalManager.on_level_complete.connect(on_level_complete)
 	SignalManager.on_game_over.connect(on_game_over)
+	SignalManager.on_player_hit.connect(on_player_hit)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -21,7 +25,11 @@ func _process(delta):
 func show_hud() -> void:
 	Engine.time_scale = 0
 	color_rect.visible = true
-
+	
+func on_player_hit(lives: int) ->void:
+	for life in range(_hearts.size()):
+		_hearts[life].visible = lives > life
+		
 func on_level_complete() -> void:
 	vb_level_complete.visible = true
 	show_hud()
